db552ab7c28bb04ff58d409c688a99003cf7c0d8
vivek sarraf <sarraf.vivek@gmail.com>
2024-03-26 20:36:03 -0400

    updated crowdfunding schema

---
+++ crowdfunding_db_sql.sql
@@ -0,0 +1,70 @@
+DROP TABLE if exists contacts;
+DROP TABLE if exists campaign;
+DROP TABLE if exists category;
+DROP TABLE if exists subcategory;
+
+ALTER DATABASE "crowdfunding_db" SET datestyle TO "ISO, MDY";
+
+
+CREATE TABLE "contacts" (
+    "contact_id" INTEGER   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "email" VARCHAR(100)   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INTEGER   NOT NULL,
+    "contact_id" INTEGER   NOT NULL,
+    "company_name" VARCHAR(100)   NOT NULL,
+    "description" VARCHAR(255)   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(5)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(10)   NOT NULL,
+    "category_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "subcategory_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+Select * from contacts;
+
+select * from category;
+
+select * from subcategory;
+
+select * from campaign;
